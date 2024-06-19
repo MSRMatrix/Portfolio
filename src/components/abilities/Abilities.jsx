1b99649feed43f8ddbfd9a8bbcb82319752faa5a
TwinMine <michaelrolo11@gmail.com>
2024-06-19 16:17:26 +0200

    first deployed push

--- src/components/abilities/Abilities.jsx
+++ src/components/abilities/Abilities.jsx
@@ -1,26 +1,40 @@
 import "./abilities.css";
 
 const Abilities = () => {
+  const skills = [
+    {
+      name: "HTML",
+      text: "Ich beherrsche HTML souverän und gestalte damit strukturierte, barrierefreie Webseiten.",
+      image: 'src/assets/html-ai.png'
+    },
+    {
+      name: "CSS",
+      text: "Meine CSS-Fähigkeiten erstrecken sich über das Design von ansprechenden und ästhetischen Webseiten.",
+      image: 'src/assets/css-ai.png'
+    },
+    {
+      name: "JavaScript",
+      text: "Meine JavaScript-Fähigkeiten umfassen die Entwicklung interaktiver Webseiten und die Implementierung von benutzerdefinierten Funktionen.",
+      image: 'src/assets/javascript.png'
+    },
+    {
+      name: "MERN Stack",
+      text: "Ich bin versiert im MERN-Stack, einschließlich MongoDB, Express.js, React und Node.js.",
+      image: 'src/assets/mern-stack.png'
+    },
+  ];
+
   return (
     <div className="information-container">
       <h1>Meine Developer Fähigkeiten</h1>
       <div className="img-container">
-        <div>
-          <h1>Html</h1>
-          <img src="src/assets/html.jpg" alt="Html Picture" />
-        </div>
-        <div>
-          <h1>Css</h1>
-          <img src="src/assets/css-small-icon.jpg" alt="Css Picture" />
-        </div>
-        <div>
-          <h1>JS</h1>
-          <img src="src/assets/javascript.jpg" alt="Javascript Picture" />
-        </div>
-        <div>
-          <h1>Mern-Stack</h1>
-          <img src="src/assets/Mern-Stack.jpeg" alt="Mern-Stack Picture" />
-        </div>
+        {skills.map((skill) => (
+          <div key={skill.name} className="picture-div">
+            <img src={skill.image} alt={skill.name} />
+            <div className="hover-text">{skill.text}</div>
+            <h2>{skill.name}</h2>
+          </div>
+        ))}
       </div>
     </div>
   );

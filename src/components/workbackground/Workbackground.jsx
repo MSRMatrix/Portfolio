bc06b90250c88b824a5de0361140d811faf85c74
TwinMine <michaelrolo11@gmail.com>
2024-07-10 14:10:14 +0200

    duh

--- src/components/workbackground/Workbackground.jsx
+++ src/components/workbackground/Workbackground.jsx
@@ -22,22 +22,22 @@ const Workbackground = () => {
     {
       info: "Berufsvorbereitungsjahr Holztechnik",
       year: "2015-2016",
-      about: "Während des Berufsvorbereitungsjahres in der Holztechnik wurde ein grundlegendes Verständnis für die Arbeit mit Holz vermittelt. Dies umfasste theoretische und praktische Kenntnisse in der Holzbearbeitung, die Bedienung von Maschinen und Werkzeugen sowie die Erstellung und Umsetzung von technischen Zeichnungen. Ziel war es, den Einstieg in handwerkliche Berufe im Holzbereich zu erleichtern und die berufliche Orientierung zu unterstützen."
+      about: "Während des Berufsvorbereitungsjahres in der Holztechnik wurde ein grundlegendes Verständnis für die Arbeit mit Holz vermittelt. Dies umfasste theoretische und praktische Kenntnisse in der Holzbearbeitung, die Bedienung von Maschinen und Werkzeugen sowie die Erstellung und Umsetzung von technischen Zeichnungen."
     },
     {
       info: "Bildungswerk der Niedersächsischen Wirtschaft gGmbH ",
       year: "2017-2018",
-      about: "Bei der Bildungswerk der Niedersächsischen Wirtschaft gGmbH wurde eine umfassende berufliche Weiterbildung absolviert. Der Fokus lag auf der beruflichen Integration und der Entwicklung von Schlüsselkompetenzen, wie z.B. Teamarbeit, Projektmanagement und Kommunikation. Zudem wurden spezifische Fachkenntnisse und Fähigkeiten in den Bereichen Wirtschaft und Technik vermittelt, um die Teilnehmer für den Arbeitsmarkt optimal vorzubereiten."
+      about: "Bei der Bildungswerk der Niedersächsischen Wirtschaft gGmbH wurde eine umfassende berufliche Weiterbildung absolviert. Der Fokus lag auf der beruflichen Integration und der Entwicklung von Schlüsselkompetenzen, wie z.B. Teamarbeit, Projektmanagement und Kommunikation."
     },
     {
       info: "Autohaus Trias GmbH",
       year: "1.08.2018 - 31.01.2023",
-      about: "Während der Tätigkeit bei der Autohaus Trias GmbH wurden umfangreiche Erfahrungen in der Automobilbranche gesammelt. Verantwortlichkeiten umfassten den Kundenservice, die Fahrzeugwartung und -reparatur sowie die Koordination von Werkstattterminen. Es wurde intensiv mit Diagnosetools gearbeitet, um technische Probleme zu identifizieren und zu beheben. Zudem wurden Kenntnisse im Bereich Verkaufsberatung und Fahrzeugfinanzierung vertieft."
+      about: "Während der Tätigkeit bei der Autohaus Trias GmbH wurden umfangreiche Erfahrungen in der Automobilbranche gesammelt. Verantwortlichkeiten umfassten den Kundenservice, die Fahrzeugwartung und -reparatur sowie die Koordination von Werkstattterminen."
     },
     {
       info: "DCI Weiterbildung zum Web- und Softwareentwickler",
       year: "16.05.2023 - 23.07.2024",
-      about: "Die Weiterbildung bei DCI konzentriert sich auf die Ausbildung zum Web- und Softwareentwickler. Der Lehrplan umfasst eine Vielzahl von Programmiersprachen und Technologien, darunter HTML, CSS, JavaScript, React, Node.js und Datenbankmanagement. Praktische Projekte und Workshops fördern die Anwendung des erlernten Wissens und bereiten die Teilnehmer auf eine Karriere in der IT-Branche vor. Schwerpunkte liegen auf der Entwicklung moderner Webanwendungen und Softwarelösungen."
+      about: "Die Weiterbildung bei DCI konzentriert sich auf die Ausbildung zum Web- und Softwareentwickler. Der Lehrplan umfasst eine Vielzahl von Programmiersprachen und Technologien, darunter HTML, CSS, JavaScript, React, Node.js und Datenbankmanagement."
     },
   ];
 

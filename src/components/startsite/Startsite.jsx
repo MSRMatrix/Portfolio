c69a586fdca4dcc721c19b1ed87e1969aed2940d
TwinMine <michaelrolo11@gmail.com>
2025-01-06 11:13:52 +0100

    change a word

--- src/components/startsite/Startsite.jsx
+++ src/components/startsite/Startsite.jsx
@@ -12,7 +12,7 @@ const Startsite = () => {
     <div className="information-container">
       <div className={`${topicDiv} startsite-style`}>
         <h1>Web Developer</h1>
-        <p>Hallo, ich bin Michael, ein erfahrener Webentwickler, der sich auf die Erstellung von maßgeschneiderten, modernen und benutzerfreundlichen Websites spezialisiert hat. Mit meiner Leidenschaft für Technologie und meinem Engagement für erstklassige Qualität helfe ich Ihnen, Ihre Online-Präsenz auf das nächste Level zu heben."</p>
+        <p>Hallo, ich bin Michael, ein motivierter Webentwickler, der sich auf die Erstellung von maßgeschneiderten, modernen und benutzerfreundlichen Websites spezialisiert hat. Mit meiner Leidenschaft für Technologie und meinem Engagement für erstklassige Qualität helfe ich Ihnen, Ihre Online-Präsenz auf das nächste Level zu heben."</p>
       </div>
     </div>
   );
